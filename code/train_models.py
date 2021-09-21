81cdb8d18ebe749ddc5dbc2cfd5017cc9766aa3f
NuwanMadhusanka <nuwanmadhusankarulz@gmail.com>
2021-09-21 15:50:39 +0530

    update the train_hopenet file

--- code/train_models.py
+++ code/train_models.py
@@ -9,14 +9,13 @@ time_list = []
 for arch in architectures:
 
     #Train with KD Loss
-    #command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 100 --w_dist 25.0 --w_angle 50.0 --change_t_s True  --arch ' + arch
-    command = 'python train_hopenet.py --dataset 300W_LP --data_dir ./datasets --filename_list_train ./datasets/300W_LP.txt --filename_list_test ./datasets/AFLW2000.txt --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 100 --w_dist 25.0 --w_angle 50.0 --change_t_s True  --arch ' + arch
+    #command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 50 --w_dist 25.0 --w_angle 50.0 --arch ' + arch
 
     #Train with Dynamic KD Loss
-    #command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 100 --w_dist 25.0 --w_angle 50.0  --kd_alpha_dynamic True --arch ' + arch
+    #command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 50 --w_dist 25.0 --w_angle 50.0  --kd_alpha_dynamic True --arch ' + arch
 
     #Train with exchange student and teacher loss
-    #command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 100 --w_dist 25.0 --w_angle 50.0 --arch ' + arch
+    command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 50 --w_dist 25.0 --w_angle 50.0 --change_t_s True  --arch ' + arch
 
     #Train with Wasserstein Distance
     #command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 100 --kd_loss ws --arch ' + arch
