a8a9d6617d6167f1e98d0f1f67746dcf131ae438
NuwanMadhusanka <nuwanmadhusankarulz@gmail.com>
2021-09-25 09:04:22 +0530

    change batch size

--- code/train_models.py
+++ code/train_models.py
@@ -1,13 +1,13 @@
 import os
 import time
 
-#architectures = ['Squeezenet_1_0', 'Squeezenet_1_1', 'ResNet34', 'ResNet18', 'MobileNetV2', 'SEResNet50', 'DenseNet201']
-architectures = ['ResNet50']
+architectures = ['ResNet50','Squeezenet_1_0', 'Squeezenet_1_1', 'ResNet34', 'ResNet18', 'MobileNetV2', 'SEResNet50']
+#architectures = ['ResNet50']
 time_list = []
 
 for arch in architectures:
 
-    command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 64 --num_epochs 50 --arch ' + arch
+    command = 'python train_hopenet.py --dataset BIWI --data_dir ./datasets/BIWI --lr 0.00001 --alpha 1 --batch_size 16 --num_epochs 100 --arch ' + arch
     
     start_time = time.time()
     os.system(command)
